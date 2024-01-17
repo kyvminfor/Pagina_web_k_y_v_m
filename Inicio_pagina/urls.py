801cb87ad62419efa61716f14436c0451588dcde
kyvminfor <Informaticakyvm@gmail.com>
2024-01-17 13:43:56 -0300

    Enlace en imagen de regreso a inicio listo

--- Inicio_pagina/urls.py
+++ Inicio_pagina/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('inicio/', views.inicio, name='inicio'),  # También mantén tu ruta existente para '/inicio/'
+    path('inicio', views.inicio, name='inicio'),  # También mantén tu ruta existente para '/inicio/'
     path('productos', views.productos, name='productos'),
     path('soluciones', views.soluciones, name='soluciones'),
     path('precios', views.precios, name='precios'),
